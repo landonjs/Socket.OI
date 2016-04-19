3b53fd283418cb9ef5a8a184583d4c0d8e1bbbbb
Landon Short <landon.short@thycotic.com>
2016-04-18 22:32:05 -0400

    Finished Setting up the Socket.IO example

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+   // console.log('message: ' + msg); sends the message to the listener
+    io.emit('chat message', msg); //sends the message to everyone includind the sender
+  });
 });
 
 http.listen(3000, function(){
